2cb42124d5e73fa3888192f1d59386813beac776
Dolly Ervina <dolly.ervina@gmail.com>
2015-07-22 15:43:45 +0700

    HELLONODEJS

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index')
+  response.send('Hello Node.js World :))')
 });
 
 app.listen(app.get('port'), function() {
